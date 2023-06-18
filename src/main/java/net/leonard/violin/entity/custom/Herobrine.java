3bb8b451cb8438dff8beade14b061b2e5118f015
Leonard Weaver <leonard.weaver45@gmail.com>
2023-06-17 23:20:04 -0500

    Cleaned up Herobrine class and added comments

--- src/main/java/net/leonard/violin/entity/custom/Herobrine.java
+++ src/main/java/net/leonard/violin/entity/custom/Herobrine.java
@@ -86,8 +86,6 @@ public class Herobrine extends Monster implements NeutralMob, GeoEntity {
 
     private static final UUID SPEED_MODIFIER_ATTACKING_UUID = UUID.fromString("020E0DFB-87AE-4653-9556-831010E291A0");
     private static final AttributeModifier SPEED_MODIFIER_ATTACKING = new AttributeModifier(SPEED_MODIFIER_ATTACKING_UUID, "Attacking speed boost", (double)0.15F, AttributeModifier.Operation.ADDITION);
-    private static final int DELAY_BETWEEN_CREEPY_STARE_SOUND = 400;
-    private static final int MIN_DEAGGRESSION_TIME = 600;
     private static final EntityDataAccessor<Optional<BlockState>> DATA_CARRY_STATE = SynchedEntityData.defineId(net.leonard.violin.entity.custom.Herobrine.class, EntityDataSerializers.OPTIONAL_BLOCK_STATE);
     private static final EntityDataAccessor<Boolean> DATA_CREEPY = SynchedEntityData.defineId(net.leonard.violin.entity.custom.Herobrine.class, EntityDataSerializers.BOOLEAN);
     private static final EntityDataAccessor<Boolean> DATA_STARED_AT = SynchedEntityData.defineId(net.leonard.violin.entity.custom.Herobrine.class, EntityDataSerializers.BOOLEAN);
@@ -106,13 +104,11 @@ public class Herobrine extends Monster implements NeutralMob, GeoEntity {
 
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(2, new LookAtPlayerGoal(this, Player.class, 500.0f, 1.0f, false));
+        this.goalSelector.addGoal(2, new LookAtPlayerGoal(this, Player.class, 500.0f, 1.0f, false)); //Stare at player for up to 500 blocks
         this.goalSelector.addGoal(1, new Herobrine.HerobrineFreezeWhenLookedAt(this));
         this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.0D, false));
         this.targetSelector.addGoal(1, new Herobrine.HerobrineLookForPlayerGoal(this, this::isAngryAt));
-//Maybe put this back in        //
         this.targetSelector.addGoal(2, new HurtByTargetGoal(this));
-        //this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Endermite.class, true, false));
         this.targetSelector.addGoal(4, new ResetUniversalAngerTargetGoal<>(this, false));
     }
 
@@ -129,7 +125,6 @@ public class Herobrine extends Monster implements NeutralMob, GeoEntity {
     public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
         controllerRegistrar.add(new AnimationController<>(this, "controller", 0, this::predicate));
     }
-
     //Animations
     private <T extends GeoAnimatable> PlayState predicate(AnimationState<T> tAnimationState) {
         if(tAnimationState.isMoving()) {
@@ -145,6 +140,8 @@ public class Herobrine extends Monster implements NeutralMob, GeoEntity {
         return cache;
     }
 
+
+
     public void setTarget(@Nullable LivingEntity p_32537_) {
         AttributeInstance attributeinstance = this.getAttribute(Attributes.MOVEMENT_SPEED);
         if (p_32537_ == null) {
@@ -159,8 +156,7 @@ public class Herobrine extends Monster implements NeutralMob, GeoEntity {
                 attributeinstance.addTransientModifier(SPEED_MODIFIER_ATTACKING);
             }
         }
-
-        super.setTarget(p_32537_); //Forge: Moved down to allow event handlers to write data manager values.
+        super.setTarget(p_32537_);
     }
 
     protected void defineSynchedData() {
@@ -196,10 +192,8 @@ public class Herobrine extends Monster implements NeutralMob, GeoEntity {
             this.lastStareSound = this.tickCount;
             if (!this.isSilent()) {
                 this.level.playLocalSound(this.getX(), this.getEyeY(), this.getZ(), ModSounds.HEROBRINE_ATTACK.get(), this.getSoundSource(), 5.0F, 1.0F, false);
-                System.out.println("HEROBRINE_START SOUND");
             }
         }
-
     }
 
     public void onSyncedDataUpdated(EntityDataAccessor<?> p_32513_) {
@@ -208,30 +202,6 @@ public class Herobrine extends Monster implements NeutralMob, GeoEntity {
         }
 
         super.onSyncedDataUpdated(p_32513_);
-    }
-
-    public void addAdditionalSaveData(CompoundTag p_32520_) {
-        super.addAdditionalSaveData(p_32520_);
-        BlockState blockstate = this.getCarriedBlock();
-        if (blockstate != null) {
-            p_32520_.put("carriedBlockState", NbtUtils.writeBlockState(blockstate));
-        }
-
-        this.addPersistentAngerSaveData(p_32520_);
-    }
-
-    public void readAdditionalSaveData(CompoundTag p_32511_) {
-        super.readAdditionalSaveData(p_32511_);
-        BlockState blockstate = null;
-        if (p_32511_.contains("carriedBlockState", 10)) {
-            blockstate = NbtUtils.readBlockState(this.level.holderLookup(Registries.BLOCK), p_32511_.getCompound("carriedBlockState"));
-            if (blockstate.isAir()) {
-                blockstate = null;
-            }
-        }
-
-        this.setCarriedBlock(blockstate);
-        this.readPersistentAngerSaveData(this.level, p_32511_);
     }
 
     boolean isLookingAtMe(Player p_32535_) {
@@ -248,6 +218,7 @@ public class Herobrine extends Monster implements NeutralMob, GeoEntity {
         }
     }
 
+    // Agro if player gets too close
     boolean isCloseToMe(Player p_32535_) {
         //Equation of a sphere used to find radius, used for setting Herobrine's spherical agro distance
         double distance = Math.sqrt(Math.pow(p_32535_.getX() - this.getX(), 2) + Math.pow(p_32535_.getZ() - this.getZ(),2) + Math.pow(p_32535_.getY() - this.getY(),2));
@@ -261,13 +232,13 @@ public class Herobrine extends Monster implements NeutralMob, GeoEntity {
         return 1.75F;
     }
 
+    //update variables on tick
     public void aiStep() {
         if (this.isAlive()) {
             boolean flag = (this.level.isDay() && !this.level.isClientSide);
             if (flag) {
                 this.teleportTo(this.getX(), -65, this.getZ()); // This essentially kills Herobrine
                 }
-
             if (!this.level.isClientSide) {
                 this.updatePersistentAnger((ServerLevel) this.level, true);
             }
@@ -275,72 +246,9 @@ public class Herobrine extends Monster implements NeutralMob, GeoEntity {
         super.aiStep(); //tick
         }
     }
-//TODO Attempt to delete this
-    public boolean isSensitiveToWater() {
-        return true;
-    }
 
-    protected void customServerAiStep() {
-        //Debugging
-
-
-        super.customServerAiStep();
-    }
-
-
-
-
-
-
-
-    /*protected SoundEvent getAmbientSound() {
-        System.out.println("HEROBRINE_SCREAM AND HEROBRINE_AMBIENT SOUND");
-        return this.isCreepy() ? SoundEvents.ENDERMAN_SCREAM : SoundEvents.ENDERMAN_AMBIENT;
-
-    }*/
-
-    /*protected SoundEvent getHurtSound(DamageSource p_32527_) {
-        System.out.println("HEROBRINE_HURT SOUND");
-        return SoundEvents.ENDERMAN_HURT;
-    }*/
-
-    /*protected SoundEvent getDeathSound() {
-        System.out.println("HEROBRINE_DEATH SOUND");
-        return SoundEvents.ENDERMAN_DEATH;
-    }*/
-
-    protected void dropCustomDeathLoot(DamageSource p_32497_, int p_32498_, boolean p_32499_) {
-        super.dropCustomDeathLoot(p_32497_, p_32498_, p_32499_);
-        BlockState blockstate = this.getCarriedBlock();
-        if (blockstate != null) {
-            ItemStack itemstack = new ItemStack(Items.DIAMOND_AXE);
-            itemstack.enchant(Enchantments.SILK_TOUCH, 1);
-            LootContext.Builder lootcontext$builder = (new LootContext.Builder((ServerLevel)this.level)).withRandom(this.level.getRandom()).withParameter(LootContextParams.ORIGIN, this.position()).withParameter(LootContextParams.TOOL, itemstack).withOptionalParameter(LootContextParams.THIS_ENTITY, this);
-
-            for(ItemStack itemstack1 : blockstate.getDrops(lootcontext$builder)) {
-                this.spawnAtLocation(itemstack1);
-            }
-        }
-
-    }
-
-    public void setCarriedBlock(@Nullable BlockState p_32522_) {
-        this.entityData.set(DATA_CARRY_STATE, Optional.ofNullable(p_32522_));
-    }
-
-    @Nullable
-    public BlockState getCarriedBlock() {
-        return this.entityData.get(DATA_CARRY_STATE).orElse((BlockState)null);
-    }
-
-
-    private boolean hurtWithCleanWater(DamageSource p_186273_, ThrownPotion p_186274_, float p_186275_) {
-        ItemStack itemstack = p_186274_.getItem();
-        Potion potion = PotionUtils.getPotion(itemstack);
-        List<MobEffectInstance> list = PotionUtils.getMobEffects(itemstack);
-        boolean flag = potion == Potions.WATER && list.isEmpty();
-        return flag ? super.hurt(p_186273_, p_186275_) : false;
-    }
+    //extra tick function not in use
+    protected void customServerAiStep() {super.customServerAiStep();}
 
     public boolean isCreepy() {
         return this.entityData.get(DATA_CREEPY);
@@ -352,10 +260,6 @@ public class Herobrine extends Monster implements NeutralMob, GeoEntity {
 
     public void setBeingStaredAt() {
         this.entityData.set(DATA_STARED_AT, true);
-    }
-
-    public boolean requiresCustomPersistence() {
-        return super.requiresCustomPersistence() || this.getCarriedBlock() != null;
     }
 
     static class HerobrineFreezeWhenLookedAt extends Goal {
@@ -384,50 +288,6 @@ public class Herobrine extends Monster implements NeutralMob, GeoEntity {
 
         public void tick() {
             this.herobrine.getLookControl().setLookAt(this.target.getX(), this.target.getEyeY(), this.target.getZ());
-        }
-    }
-
-    static class HerobrineLeaveBlockGoal extends Goal {
-        private final net.leonard.violin.entity.custom.Herobrine herobrine;
-
-        public HerobrineLeaveBlockGoal(net.leonard.violin.entity.custom.Herobrine p_32556_) {
-            this.herobrine = p_32556_;
-        }
-
-        public boolean canUse() {
-            if (this.herobrine.getCarriedBlock() == null) {
-                return false;
-            } else if (!net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(this.herobrine.level, this.herobrine)) {
-                return false;
-            } else {
-                return this.herobrine.getRandom().nextInt(reducedTickDelay(2000)) == 0;
-            }
-        }
-
-        public void tick() {
-            RandomSource randomsource = this.herobrine.getRandom();
-            Level level = this.herobrine.level;
-            int i = Mth.floor(this.herobrine.getX() - 1.0D + randomsource.nextDouble() * 2.0D);
-            int j = Mth.floor(this.herobrine.getY() + randomsource.nextDouble() * 2.0D);
-            int k = Mth.floor(this.herobrine.getZ() - 1.0D + randomsource.nextDouble() * 2.0D);
-            BlockPos blockpos = new BlockPos(i, j, k);
-            BlockState blockstate = level.getBlockState(blockpos);
-            BlockPos blockpos1 = blockpos.below();
-            BlockState blockstate1 = level.getBlockState(blockpos1);
-            BlockState blockstate2 = this.herobrine.getCarriedBlock();
-            if (blockstate2 != null) {
-                blockstate2 = Block.updateFromNeighbourShapes(blockstate2, this.herobrine.level, blockpos);
-                if (this.canPlaceBlock(level, blockpos, blockstate2, blockstate, blockstate1, blockpos1) && !net.minecraftforge.event.ForgeEventFactory.onBlockPlace(herobrine, net.minecraftforge.common.util.BlockSnapshot.create(level.dimension(), level, blockpos1), net.minecraft.core.Direction.UP)) {
-                    level.setBlock(blockpos, blockstate2, 3);
-                    level.gameEvent(GameEvent.BLOCK_PLACE, blockpos, GameEvent.Context.of(this.herobrine, blockstate2));
-                    this.herobrine.setCarriedBlock((BlockState)null);
-                }
-
-            }
-        }
-
-        private boolean canPlaceBlock(Level p_32559_, BlockPos p_32560_, BlockState p_32561_, BlockState p_32562_, BlockState p_32563_, BlockPos p_32564_) {
-            return p_32562_.isAir() && !p_32563_.isAir() && !p_32563_.is(Blocks.BEDROCK) && !p_32563_.is(net.minecraftforge.common.Tags.Blocks.ENDERMAN_PLACE_ON_BLACKLIST) && p_32563_.isCollisionShapeFullBlock(p_32559_, p_32564_) && p_32561_.canSurvive(p_32559_, p_32560_) && p_32559_.getEntities(this.herobrine, AABB.unitCubeFromLowerCorner(Vec3.atLowerCornerOf(p_32560_))).isEmpty();
         }
     }
 
@@ -479,34 +339,28 @@ public class Herobrine extends Monster implements NeutralMob, GeoEntity {
                     if (this.herobrine.hasIndirectPassenger(this.target)) {
                         return false;
                     }
-
                     if (this.continueAggroTargetConditions.test(this.herobrine, this.target)) {
                         return true;
                     }
                 }
-
                 return super.canContinueToUse();
             }
         }
 
+        //Once Herobrine finishes chasing player he will despawn
         public void isTooCloseToMe() {
             //Equation of a sphere used to find radius, used for setting Herobrine's spherical agro distance
             double distance = Math.sqrt(Math.pow(target.getX() - this.herobrine.getX(), 2) + Math.pow(target.getZ() - this.herobrine.getZ(),2) + Math.pow(target.getY() - this.herobrine.getY(),2));
-            System.out.println("Player: " + target.getX() + ", " + target.getY() + ", " + target.getZ());
-            System.out.println("Herobrine: " + this.herobrine.getX() + ", " + this.herobrine.getY() + ", " + this.herobrine.getZ());
             if (distance < 1.5) {
-                System.out.println("TOO CLOSE");
                 this.herobrine.teleportTo(this.herobrine.getX(), -65, this.herobrine.getZ());
             }
         }
 
+        //Update target
         public void tick() {
-            System.out.println("tick");
-
             if (this.herobrine.getTarget() == null) {
                 super.setTarget((LivingEntity)null);
             }
-
             if (this.pendingTarget != null) {
                 if (--this.aggroTime <= 0) {
                     this.target = this.pendingTarget;
@@ -514,56 +368,13 @@ public class Herobrine extends Monster implements NeutralMob, GeoEntity {
                     super.start();
                 }
             }
-
             if (this.target != null) {
                 this.isTooCloseToMe();
                 super.tick();
             }
-
             else {
-
-
                 super.tick();
             }
-
-        }
-    }
-
-    static class HerobrineTakeBlockGoal extends Goal {
-        private final net.leonard.violin.entity.custom.Herobrine herobrine;
-
-        public HerobrineTakeBlockGoal(net.leonard.violin.entity.custom.Herobrine p_32585_) {
-            this.herobrine = p_32585_;
-        }
-
-        public boolean canUse() {
-            if (this.herobrine.getCarriedBlock() != null) {
-                return false;
-            } else if (!net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(this.herobrine.level, this.herobrine)) {
-                return false;
-            } else {
-                return this.herobrine.getRandom().nextInt(reducedTickDelay(20)) == 0;
-            }
-        }
-
-        public void tick() {
-            RandomSource randomsource = this.herobrine.getRandom();
-            Level level = this.herobrine.level;
-            int i = Mth.floor(this.herobrine.getX() - 2.0D + randomsource.nextDouble() * 4.0D);
-            int j = Mth.floor(this.herobrine.getY() + randomsource.nextDouble() * 3.0D);
-            int k = Mth.floor(this.herobrine.getZ() - 2.0D + randomsource.nextDouble() * 4.0D);
-            BlockPos blockpos = new BlockPos(i, j, k);
-            BlockState blockstate = level.getBlockState(blockpos);
-            Vec3 vec3 = new Vec3((double)this.herobrine.getBlockX() + 0.5D, (double)j + 0.5D, (double)this.herobrine.getBlockZ() + 0.5D);
-            Vec3 vec31 = new Vec3((double)i + 0.5D, (double)j + 0.5D, (double)k + 0.5D);
-            BlockHitResult blockhitresult = level.clip(new ClipContext(vec3, vec31, ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE, this.herobrine));
-            boolean flag = blockhitresult.getBlockPos().equals(blockpos);
-            if (blockstate.is(BlockTags.ENDERMAN_HOLDABLE) && flag) {
-                level.removeBlock(blockpos, false);
-                level.gameEvent(GameEvent.BLOCK_DESTROY, blockpos, GameEvent.Context.of(this.herobrine, blockstate));
-                this.herobrine.setCarriedBlock(blockstate.getBlock().defaultBlockState());
-            }
-
         }
     }
 }
